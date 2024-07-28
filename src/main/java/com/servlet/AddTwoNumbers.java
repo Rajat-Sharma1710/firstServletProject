1030fa69ad70d7a4939a7f44756ad42e051d85c9
rajatSharma <rajatsharma@Rajats-MacBook-Air.local>
2024-07-29 00:17:34 +0530

    Adding First Servlet Project having RequestDispacher, Session, SendRedirect

---
+++ src/main/java/com/servlet/AddTwoNumbers.java
@@ -0,0 +1,41 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/*
+ * 1. Servlet class should be identified as class that extends HttpServlet
+ * 2. service(req, res) is the method that does the processing with the incoming
+ * 	  request and returns the response.
+ */
+public class AddTwoNumbers extends HttpServlet{
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int first = Integer.parseInt(req.getParameter("num1"));
+		int sec = Integer.parseInt(req.getParameter("num2"));
+		
+		int sum = first + sec;
+//		req.setAttribute("sum", sum);
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("k", sum);
+		
+		// sends the current servlet to another servlet named squ
+//		RequestDispatcher obj = req.getRequestDispatcher("sq");
+//		obj.forward(req, res);
+		
+		// send a response back to the client to redirect to a different servlet
+		// send the value of sum as a parameter in query string
+//		res.sendRedirect("sq?k=" + sum);
+		
+		res.sendRedirect("sq");
+	}
+
+}
---
+++ src/main/java/com/servlet/SquareOfServlet.java
@@ -0,0 +1,27 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SquareOfServlet extends HttpServlet {
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		// get the req data which comes from the request of another servlet
+//		int resfromPrevServlet = (int) req.getAttribute("sum");
+		
+		// since we don't get the data from the same request obj
+		HttpSession session = req.getSession();
+		int sq = (int) session.getAttribute("k");
+		
+		int result = sq * sq;
+		PrintWriter out = res.getWriter();
+		out.println("The Square of sum is: " + result);
+	}
+
+
+}
